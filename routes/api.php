a5ca669d012d4453225b4dac4fee040cf41b7145
wmDeveloper3 <wmdevloper3@awalan-qa.com>
2021-04-21 16:56:59 +0530

    Init project

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('country', 'Country\CountryController@country');
+
+Route::get('country/{id}', 'Country\CountryController@countryById');
+
+Route::post('country', 'Country\CountryController@countrySave');
+
+Route::put('country/{country}', 'Country\CountryController@countryUpdate');
+
+Route::delete('country/{country}', 'Country\CountryController@countryDelete');
